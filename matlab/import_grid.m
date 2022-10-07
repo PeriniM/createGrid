688afccd2292b7da03f7febe7f55b722ace23c4a
Marco Perini <perinim.98@gmail.com>
2022-10-07 15:48:41 +0200

    fixed matlab import

--- matlab/import_grid.m
+++ matlab/import_grid.m
@@ -1,16 +1,17 @@
 %% takes the elements and plot them on a grid
 clear
 clc
-T = readtable('createGrid().csv');
+T = readcell('createGrid().csv');
 
 hold on
-for i = 1:height(T)
-    x = str2num(string(T{i,"x_vert"}));
-    y = str2num(string(T{i,"y_vert"}));
+for i = 2:height(T)
+    x = str2num(string(T(i,2)));
+    y = str2num(string(T(i,3)));
     plot([x, x(1)],[y, y(1)])
     %patch(x,y,'g')
 end
 axis ij
+daspect([1 1 1])
 grid on
 grid minor
 hold off
